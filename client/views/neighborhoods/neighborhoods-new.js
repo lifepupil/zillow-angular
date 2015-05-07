ce738603604b2d5679d219bd8f6d23bcae2542f6
Christian Richard <lifepupil@gmail.com>
2015-05-07 01:47:54 -0700

    Evan commit

--- client/views/neighborhoods/neighborhoods-new.js
+++ client/views/neighborhoods/neighborhoods-new.js
@@ -5,23 +5,22 @@
 angular.module('poseidon')
 .controller('NewNeighborhoodCtrl', function($scope, Map, Neighborhood){
 
-
   $scope.createNeighborhood = function(neighborhood) {
-
     Map.geocode(neighborhood.name, function(results) {
-
-      console.log('results', results);
       if(results && results.length) {
+        var neighborhood = {};
         neighborhood.name = results[0].formatted_address;
         neighborhood.lat = results[0].geometry.location.lat();
         neighborhood.lng = results[0].geometry.location.lng();
+        console.log(neighborhood);
 
-        var neighborhood = new Neighborhood($scope.neighborhood);
-        neighborhood.addStop(neighborhood)
-        .then(function(response) {
-          $scope.neighborhood = response.data;
-          // addMarkers();
-        });
+        Neighborhood.create(neighborhood);
+
+        // neighborhood.addStop(neighborhood)
+        // .then(function(response) {
+        //   $scope.neighborhood = response.data;
+          // addMarkers()
+      // });
       }
 
     });
